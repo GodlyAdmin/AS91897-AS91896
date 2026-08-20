24771b6b2ef9c92431356b02e348faf0e4054f2d
Admin <ghimireadmen@gmail.com>
2026-08-20 17:53:04 +1200

    added the python code to open each file, the basic css code for the homepage and all of the files for the websites basic pages.

---
+++ AS91897-AS91896-main/app.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template
+ 
+app = Flask(__name__)
+ 
+ 
+@app.route("/")
+def home():
+    return render_template("Index.html")
+ 
+ 
+@app.route("/menu")
+def menu():
+    return render_template("menu.html")
+ 
+ 
+@app.route("/orders")
+def order_history():
+    return render_template("order_history.html")
+ 
+ 
+@app.route("/checkout")
+def checkout():
+    return render_template("checkout.html")
+ 
+ 
+if __name__ == "__main__":
+    app.run(debug=True)
+ 
